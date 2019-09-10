265a0434f292e48ba3fb83389801bee0b9f2fbdc
AstridLL <Astridll@Astrids-MacBook-Pro-3.local>
2019-09-10 13:26:00 +0200

    first commut

---
+++ Python intro Kenneth VS code.py
@@ -0,0 +1,32 @@
+# Python notes with Kenneth
+# Notes (Version 3.6 or above)
+# Everything is UTF-8 
+# Editor: Visual studio code
+# Open source all purpose text editor from Microsoft
+# Code overview
+
+d = 14
+#%%
+
+# Use Pip or pip3 for installing packages
+# install form git (See slides)
+
+
+2+2
+
+# make string regarding westworld that uses 
+# addition
+# multiplikation
+# at least one object method
+# print the stirng 
+# make a print function that takes 
+# two numbers and print the sum as well as the
+#  calculation e.g. given 2 and 3 it would 
+# print (2+3=5
+
+westworld = "This is Westworld"
+print(westworld + " and so many robots")
+how_many = "lots "
+print(westworld + " and so many robots " + how_many.upper()*3)
+how_many.upper()
+#%%
